3c58aaceba123b70aa77cbd95bbe3e362be581d0
Phạm Nguyễn Mỹ Diễm <mydiem1802@gmail.com>
2021-04-09 15:11:43 +0700

    Initial Search History

---
+++ src/com/InterfaceWordPanel/HistoryWord.java
@@ -0,0 +1,11 @@
+package com.InterfaceWordPanel;
+
+import javax.swing.*;
+
+/**
+ * @author Pham Nguyen My Diem
+ * @version 1.0
+ * @date 4/9/2021 3:10 PM
+ */
+public class HistoryWord extends JPanel {
+}
